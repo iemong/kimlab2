ede6836e56588cbb836bbad0addbdc23cf13ff20
入江信之介 <(kiyoshidainagon@gmail.com)>
2015-06-22 21:58:02 +0900

    トップ完成と画像のパスを修正

--- assets/js/top.js
+++ assets/js/top.js
@@ -44,6 +44,15 @@ $(function(){
 	}
 
 	$('#wrapper').tubular({
-		videoId: 'Qv3bPuh_baY'
+		videoId: 'jBfzUanJCd4'
+	});
+
+	$('#js-top__next').hover(
+		function(){
+			$(".fadeInImage").animate({opacity:1.0},500);
+		},
+		function(){
+			$(".fadeInImage").animate({opacity:0.0},500);
 		});
+
 });
